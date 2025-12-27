8eb65ce50d3fd066be960a45e6a90cd463347351
happycoder4ever <reyeslorin10@gmail.com>
2025-12-27 02:38:33 +0100

    fix: changed PrismaClient import path

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../prisma/generated";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
 const adapter = new PrismaBetterSqlite3({
